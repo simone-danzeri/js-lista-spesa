f0d865257ae84e450919860f98a4f948e1191ad0
simone-danzeri <s.danzeri@icloud.com>
2024-03-13 13:27:38 +0100

    feat: Ciclo For

--- FOR/js/script.js
+++ FOR/js/script.js
@@ -1,5 +1,5 @@
 // Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
-
+// Questa è la mia lista
 const list = [
     'Latte',
     'Cacao',
@@ -8,3 +8,14 @@ const list = [
     'Pomodori',
     'Piatti pronti'
 ];
+
+// Devo stampare in pagina gli elementi della mia lista
+
+// Seleziono dove andare a stampare
+let myLi = document.querySelector('#list');
+console.log(myLi);
+
+for (let i = 0; i < list.length; i++) {
+    myLi.innerHTML += `<li>${list[i]}</li>`;
+    // console.log(list[i]);
+}
--- WHILE/js/script.js
+++ WHILE/js/script.js
@@ -1,5 +1,5 @@
 // Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
-
+// Questa è la mia lista
 const list = [
     'Latte',
     'Cacao',
