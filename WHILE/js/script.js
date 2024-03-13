8fa3ffa963b4dae0910fd85b00ac03d3b43c46f0
simone-danzeri <s.danzeri@icloud.com>
2024-03-13 13:10:29 +0100

    first commit

---
+++ WHILE/js/script.js
@@ -0,0 +1,10 @@
+// Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
